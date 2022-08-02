74f5d4c4253fef2a126f6c45ba72369e5f9f5a6f
Jorge Barcasnegras <jorgebarcasnegras@gmail.com>
2022-08-01 20:34:34 -0500

    se agrega la funcion suma

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def run():
-    print('hola mundo')
-    print('esta es una prueba')
+    print(' hola mundo ')
+    print(' esta es una prueba ')
+    num1= int(input("Dame tu numero "))
+    num2= int(input('Dame otro numero '))
+    respuesta= num1+num2
+    respuesta= str(respuesta)
+    print('la suma de ambos numeros es ' + respuesta)
 if __name__ == '__main__':
     run()
 
